bbe3612012f75e51f53ef4f5ac579ec7d87b991f
Samir <30921483+SamirAhma@users.noreply.github.com>
2019-09-21 20:08:53 +0800

    Connected To Contentful

---
+++ src/components/Global/footer.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default function Footer() {
+  return (
+    <footer className="footer py-3">
+      <div className="container">
+        <div className="row">
+          <h1 className="col-10 mx-auto col-md-6 text-yellow text-center text-capitalize">
+            All Right Reserved @2019
+          </h1>
+        </div>
+      </div>
+    </footer>
+  )
+}
